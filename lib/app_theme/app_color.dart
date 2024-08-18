e58e0ed0c5c8366a824f2be8644fd60fc1bdae99
Muhammad Arafat <arafat.softifybd@gmail.com>
2024-08-18 21:28:46 +0600

    color add push

---
+++ lib/app_theme/app_color.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color textColor = Color(0xFF6F6F6F);
+  static const Color accentColor = Color(0xFFDEF0DE);
+  static const Color white = Colors.white;
+  static const Color textThemeColor = Color.fromARGB(255, 78, 100, 144);
+  static const Color draft = Color(0xFF9E9E9E);
+  static const Color inProcess = Color(0xFFFFC107);
+  static const Color approved = Color(0xFF4CAF50);
+  static const Color completed = Color(0xFF388E3C);
+  static const Color ordered = Color(0xFF2196F3);
+  static const Color pending = Color(0xFFFF9800);
+  static const Color delivered = Color(0xFFFF7B00);
+  static const Color invoiced = Color(0xFF388E3C);
+  static const Color partiallyDelivered = Color(0xFFFFC107);
+  static const Color partiallyInvoiced = Color(0xFFFFC107);
+  static const Color notInvoiced = Color(0xFF9E9E9E);
+  static const Color pickConfirmed = Color(0xFF4CAF50);
+  static const Color paid = Color(0xFF388E3C);
+  static const Color cancelled = Color(0xFF395068);
+  static const Color overDue = Color(0xFFDC3545);
+  static const Color partiallyPaid = Color(0xFFFFC107);
+  static const Color unpaid = Color(0xFFDC3545);
+  static const Color verified = Color(0xFF4CAF50);
+  static const Color refunded = Color(0xFF388E3C);
+  static const Color received = Color(0xFF388E3C);
+  static const Color initiated = Color(0xFF9E9E9E);
+  static const Color active = Color(0xFF4CAF50);
+  static const Color inactive = Color(0xFFDC3545);
+  static const Color closed = Color(0xFF388E3C);
+  static const Color notBillable = Color(0xFF9E9E9E);
+  
+  // New colors
+  static const Color deepPurple = Color(0xFF1A237E);  // #1A237E
+  static const Color skyBlue = Color(0xFF87CEEB);     // #87CEEB
+  static const Color lightGray = Color(0xFFF5F5F5);   // #F5F5F5
+}
