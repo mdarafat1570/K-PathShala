b9095bb6cce9ef91d87b4a90ae6fc0655364c492
Fayaz <eragamer2345@gmail.com>
2024-08-29 21:46:05 +0600

    Exam page

--- lib/view/Exam Main page/bottom_sheet.dart
+++ lib/view/Exam Main page/bottom_sheet.dart
@@ -1,4 +1,7 @@
-import 'package:kpathshala/app_base/common_imports.dart';
+import 'package:flutter/material.dart';
+import 'package:kpathshala/app_theme/app_color.dart';
+import 'package:kpathshala/view/common_widget/common_button_add.dart';
+import 'package:kpathshala/view/common_widget/custom_text.dart.dart';
 
 class BottomSheetPage extends StatefulWidget {
   const BottomSheetPage({super.key});
@@ -10,106 +13,109 @@ class BottomSheetPage extends StatefulWidget {
 class _BottomSheetPageState extends State<BottomSheetPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Flexible(
-          child: Divider(
-            color: Color.fromARGB(255, 217, 217, 217),
-            thickness: 4,
-            indent: 150,
-            endIndent: 150,
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+      child: Column(
+        children: [
+          Flexible(
+            child: Divider(
+              color: Color.fromARGB(255, 217, 217, 217),
+              thickness: 4,
+              indent: screenWidth * 0.3,
+              endIndent: screenWidth * 0.3,
+            ),
           ),
-        ),
-        Gap(30),
-        customText("Confirm purchase", TextType.paragraphTitle),
-        Gap(30),
-        Container(
-          width: 370,
-          height: 92,
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(241, 239, 239, 1),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10, top: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Payment of ৳999.00",
-                  style: TextStyle(
-                    color: AppColor.navyBlue,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Gap(5),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: RichText(
-                    text: TextSpan(
-                      text: "You’ll have access to UBT Mock Test till ",
-                      style: TextStyle(
-                        color: Colors.grey, // Color for the first part
-                        fontSize: 15,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: "12th January 2025",
-                          style: TextStyle(
-                            color: AppColor
-                                .navyBlue, // Different color for the date
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
+          SizedBox(height: screenHeight * 0.03),
+          customText("Confirm purchase", TextType.paragraphTitle),
+          SizedBox(height: screenHeight * 0.03),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(241, 239, 239, 1),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Payment of ৳999.00",
+                    style: TextStyle(
+                      color: AppColor.navyBlue,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Gap(30),
-        commonCustomButton(
-          width: 370,
-          backgroundColor: AppColor.navyBlue,
-          height: 60,
-          borderRadius: 30,
-          onPressed: () {},
-          reversePosition: false,
-          child: Text(
-            "Proceed to payment",
-            style: TextStyle(color: AppColor.white, fontSize: 20),
-          ),
-        ),
-        Gap(30),
-        SizedBox(
-          width: 400,
-          child: RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-              text:
-                  "By proceeding you’re agreeing with K-Pathshala’s purchasing ",
-              style: TextStyle(
-                color: AppColor.black, // Main color for the text
-                fontSize: 14,
-              ),
-              children: [
-                TextSpan(
-                  text: "and refund policy.",
-                  style: TextStyle(
-                    color: AppColor
-                        .black, // Keep the same color or change as needed
-                    fontSize: 14,
+                  SizedBox(height: screenHeight * 0.01),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "You’ll have access to UBT Mock Test till ",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "12th January 2025",
+                            style: TextStyle(
+                              color: AppColor.navyBlue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(height: screenHeight * 0.03),
+          commonCustomButton(
+            width: double.infinity,
+            backgroundColor: AppColor.navyBlue,
+            height: 60,
+            borderRadius: 30,
+            onPressed: () {},
+            reversePosition: false,
+            child: Text(
+              "Proceed to payment",
+              style: TextStyle(color: AppColor.white, fontSize: 20),
+            ),
+          ),
+          SizedBox(height: screenHeight * 0.03),
+          SizedBox(
+            width: double.infinity,
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text:
+                    "By proceeding you’re agreeing with K-Pathshala’s purchasing ",
+                style: TextStyle(
+                  color: AppColor.black,
+                  fontSize: 14,
+                ),
+                children: [
+                  TextSpan(
+                    text: "and refund policy.",
+                    style: TextStyle(
+                      color: AppColor.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
