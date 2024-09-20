f60cde188bf6342f680fa8f9eb522ee72d32eba1
Muhammad Arafat <arafat.softifybd@gmail.com>
2024-09-20 08:57:30 +0600

    issue solve

--- lib/repository/dashboard_repository/dashboard_page_repository.dart
+++ lib/repository/dashboard_repository/dashboard_page_repository.dart
@@ -2,13 +2,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:kpathshala/api/api_container.dart';
 import 'package:kpathshala/repository/authentication_repository.dart';
+
 import '../../model/dashboard_page_model/dashboard_page_model.dart';
+
 
 class DashboardRepository {
   final AuthService _authService = AuthService();
 
   // Fetch Dashboard Data
-  Future<List<DashboardPageModel>?> fetchDashboardData() async {
+  Future<DashboardPageModel?> fetchDashboardData() async {
     final url = Uri.parse(KpatshalaDashboardPage.dashboard);
     final token = await _authService.getToken();  // Get auth token
 
@@ -25,20 +27,13 @@ class DashboardRepository {
       final response = await http.get(url, headers: headers);
 
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        if (data is List) {
-          // Parse each item in the list and return a list of models
-          return data.map<DashboardPageModel>((item) =>
-            DashboardPageModel.fromJson(item as Map<String, dynamic>)).toList();
-        } else {
-          throw Exception('Expected a list but got a ${data.runtimeType}');
-        }
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        return DashboardPageModel.fromJson(data);
       } else if (response.statusCode == 401) {
         // Handle unauthorized error
         throw Exception('Unauthorized: Token expired or invalid.');
       } else {
-        // Other types of status code errors
-        throw Exception('Failed to load dashboard data. Status code: ${response.statusCode}');
+        throw Exception('Failed to load dashboard data.');
       }
     } catch (e) {
       throw Exception('Error fetching dashboard data: ${e.toString()}');
