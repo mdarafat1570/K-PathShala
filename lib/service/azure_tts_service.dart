235be548841c3e3bf6556f69ca230e038ccd90a2
mahmudebnezaman <mahmudebnejaman@gmail.com>
2024-10-20 23:33:51 +0600

    audio related services added

--- lib/service/azure_tts_service.dart
+++ lib/service/azure_tts_service.dart
@@ -1,12 +1,16 @@
 import 'package:http/http.dart' as http;
+import 'dart:typed_data';
 
 class AzureTTS {
-  final String apiKey = 'efef202bedc846ad81274b6e3c3107da'; // Your Azure resource key
-  final String region = 'southeastasia'; // Your Azure region
+  final String apiKey = 'efef202bedc846ad81274b6e3c3107da';
+  final String region = 'southeastasia';
 
+  final String maleVoiceName = 'ko-KR-HyunsuMultilingualNeural';
+  final String femaleVoiceName = 'ko-KR-SoonBokNeural';
 
   Future<String> _fetchToken() async {
     final url = 'https://$region.api.cognitive.microsoft.com/sts/v1.0/issueToken';
+
     final response = await http.post(
       Uri.parse(url),
       headers: {
@@ -14,6 +18,7 @@ class AzureTTS {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
     );
+
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -21,12 +26,13 @@ class AzureTTS {
     }
   }
 
-
-  Future<void> speak(String text, String voiceName) async {
+  // Synthesize speech and return the audio as Uint8List
+  Future<Uint8List> synthesizeSpeech(String text, String gender) async {
+    String voiceName = gender == 'male' ? maleVoiceName : femaleVoiceName;
     final token = await _fetchToken();
     final url = 'https://$region.tts.speech.microsoft.com/cognitiveservices/v1';
 
-
+    // Headers for the API request
     final headers = {
       'Authorization': 'Bearer $token',
       'Content-Type': 'application/ssml+xml',
@@ -34,8 +40,7 @@ class AzureTTS {
       'User-Agent': 'flutter-azure-tts',
     };
 
-
-
+    // XML body for SSML (Speech Synthesis Markup Language)
     final body = '''
     <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="ko-KR">
         <voice name="$voiceName">
@@ -46,20 +51,18 @@ class AzureTTS {
     </speak>
     ''';
 
-
+    // Send the POST request to synthesize speech
     final response = await http.post(
       Uri.parse(url),
       headers: headers,
       body: body,
     );
 
-
+    // Check if the request was successful
     if (response.statusCode == 200) {
-      final audioPlayer = AudioPlayer();
-      await audioPlayer.play(BytesSource(response.bodyBytes));
+      return response.bodyBytes; // Return the audio data
     } else {
       throw Exception('Failed to synthesize speech: ${response.body}');
     }
   }
 }
-
