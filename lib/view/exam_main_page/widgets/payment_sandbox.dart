2f5d96be17042b651dc92e65b0a98fab6c3516c7
mahmudebnezaman <mahmudebnejaman@gmail.com>
2024-10-20 14:52:21 +0600

    payment related issue solved

--- lib/view/exam_main_page/widgets/payment_sandbox.dart
+++ lib/view/exam_main_page/widgets/payment_sandbox.dart
@@ -1,26 +1,13 @@
 import 'dart:developer';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_sslcommerz/model/SSLCAdditionalInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCCustomerInfoInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCEMITransactionInitializer.dart';
 import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
-import 'package:flutter_sslcommerz/model/SSLCShipmentInfoInitializer.dart';
 import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
 import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
 import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
-import 'package:flutter_sslcommerz/model/sslproductinitilizer/General.dart';
-import 'package:flutter_sslcommerz/model/sslproductinitilizer/SSLCProductInitializer.dart';
 import 'package:flutter_sslcommerz/sslcommerz.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:kpathshala/app_theme/app_color.dart';
-import 'package:kpathshala/model/log_in_credentials.dart';
-import 'package:kpathshala/repository/authentication_repository.dart';
 import 'package:kpathshala/repository/payment/payment_repository.dart';
-import 'package:kpathshala/view/common_widget/common_button_add.dart';
 import 'package:kpathshala/view/exam_main_page/quiz_attempt_page/quiz_attempt_page_imports.dart';
-
-enum SdkType { TESTBOX, LIVE }
 
 class SSLCommerzPage extends StatefulWidget {
   final int packageId;
@@ -37,7 +24,6 @@ class SSLCommerzPageState extends State<SSLCommerzPage> {
   String transactionID = 'KP+${DateTime.now().second}';
 
   dynamic formData = {};
-  SdkType _radioSelected = SdkType.TESTBOX;
 
   @override
   void initState() {
@@ -48,7 +34,6 @@ class SSLCommerzPageState extends State<SSLCommerzPage> {
     formData['phone'] = "";
     formData['amount'] = 10.0;
     formData['multicard'] = '';
-    _radioSelected = SdkType.LIVE;
   }
   Future<void> readCredentials() async {
     credentials = await _authService.getLogInCredentials();
@@ -108,131 +93,7 @@ class SSLCommerzPageState extends State<SSLCommerzPage> {
       ),
     );
     SSLCTransactionInfoModel result = await sslcommerz.payNow();
-    paymentStatusCheck(result);
-  }
-
-  Future<void> sslCommerzCustomizedCall() async {
-    Sslcommerz sslcommerz = Sslcommerz(
-      initializer: SSLCommerzInitialization(
-        //Use the ipn if you have valid one, or it will fail the transaction.
-        ipn_url: "www.ipnurl.com",
-        multi_card_name: formData['multicard'],
-        currency: SSLCurrencyType.BDT,
-        product_category: "Food",
-        sdkType: _radioSelected == SdkType.TESTBOX
-            ? SSLCSdkType.TESTBOX
-            : SSLCSdkType.LIVE,
-        store_id: formData['store_id'],
-        store_passwd: formData['store_password'],
-        total_amount: formData['amount'],
-        tran_id: "1231321321321312",
-      ),
-    );
-
-    sslcommerz
-        .addEMITransactionInitializer(
-            sslcemiTransactionInitializer: SSLCEMITransactionInitializer(
-                emi_options: 1, emi_max_list_options: 9, emi_selected_inst: 0))
-        .addShipmentInfoInitializer(
-            sslcShipmentInfoInitializer: SSLCShipmentInfoInitializer(
-                shipmentMethod: "yes",
-                numOfItems: 5,
-                shipmentDetails: ShipmentDetails(
-                    shipAddress1: "Ship address 1",
-                    shipCity: "Faridpur",
-                    shipCountry: "Bangladesh",
-                    shipName: "Ship name 1",
-                    shipPostCode: "7860")))
-        .addCustomerInfoInitializer(
-          customerInfoInitializer: SSLCCustomerInfoInitializer(
-            customerState: "Chattogram",
-            customerName: "Abu Sayed Chowdhury",
-            customerEmail: "abc@gmail.com",
-            customerAddress1: "Anderkilla",
-            customerCity: "Chattogram",
-            customerPostCode: "200",
-            customerCountry: "Bangladesh",
-            customerPhone: formData['phone'],
-          ),
-        )
-        .addProductInitializer(
-            sslcProductInitializer:
-                // ***** ssl product initializer for general product STARTS*****
-                SSLCProductInitializer(
-          productName: "Water Filter",
-          productCategory: "Widgets",
-          general: General(
-            general: "General Purpose",
-            productProfile: "Product Profile",
-          ),
-        )
-            // ***** ssl product initializer for general product ENDS*****
-
-            // ***** ssl product initializer for non physical goods STARTS *****
-            // SSLCProductInitializer.WithNonPhysicalGoodsProfile(
-            //     productName:
-            //   "productName",
-            //   productCategory:
-            //   "productCategory",
-            //   nonPhysicalGoods:
-            //   NonPhysicalGoods(
-            //      productProfile:
-            //       "Product profile",
-            //     nonPhysicalGoods:
-            //     "non physical good"
-            //       ))
-            // ***** ssl product initializer for non physical goods ENDS *****
-
-            // ***** ssl product initialization for travel vertices STARTS *****
-            //       SSLCProductInitializer.WithTravelVerticalProfile(
-            //          productName:
-            //         "productName",
-            //         productCategory:
-            //         "productCategory",
-            //         travelVertical:
-            //         TravelVertical(
-            //               productProfile: "productProfile",
-            //               hotelName: "hotelName",
-            //               lengthOfStay: "lengthOfStay",
-            //               checkInTime: "checkInTime",
-            //               hotelCity: "hotelCity"
-            //             )
-            //       )
-            // ***** ssl product initialization for travel vertices ENDS *****
-
-            // ***** ssl product initialization for physical goods STARTS *****
-
-            // SSLCProductInitializer.WithPhysicalGoodsProfile(
-            //     productName: "productName",
-            //     productCategory: "productCategory",
-            //     physicalGoods: PhysicalGoods(
-            //         productProfile: "Product profile",
-            //         physicalGoods: "non physical good"))
-
-            // ***** ssl product initialization for physical goods ENDS *****
-
-            // ***** ssl product initialization for telecom vertice STARTS *****
-            // SSLCProductInitializer.WithTelecomVerticalProfile(
-            //     productName: "productName",
-            //     productCategory: "productCategory",
-            //     telecomVertical: TelecomVertical(
-            //         productProfile: "productProfile",
-            //         productType: "productType",
-            //         topUpNumber: "topUpNumber",
-            //         countryTopUp: "countryTopUp"))
-            // ***** ssl product initialization for telecom vertice ENDS *****
-            )
-        .addAdditionalInitializer(
-          sslcAdditionalInitializer: SSLCAdditionalInitializer(
-            valueA: "value a ",
-            valueB: "value b",
-            valueC: "value c",
-            valueD: "value d",
-            extras: {"key": "key", "key2": "key2"},
-          ),
-        );
-
-    SSLCTransactionInfoModel result = await sslcommerz.payNow();
+    log(jsonEncode(result));
     paymentStatusCheck(result);
   }
 
