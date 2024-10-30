1d5c29e1af2e3ea5356dc5775654cbdc3bc9e354
mahmudebnezaman <mahmudebnejaman@gmail.com>
2024-10-31 00:19:03 +0600

    added playing indicator

--- lib/service/audio_playback_service.dart
+++ lib/service/audio_playback_service.dart
@@ -1,18 +1,18 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:ui';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:path_provider/path_provider.dart';
 
 class AudioPlaybackService {
   final AudioPlayer _audioPlayer = AudioPlayer();
-
   final double _speed = 1.0;
   final double _volume = 1.0;
 
   List<String> _audioQueue = [];
   bool _isPlaying = false;
   bool _shouldStop = false;
+
+  String? currentPlayingAudioId; // To store the extracted ID
 
   Future<void> playAudioQueue(List<String> audioKeys) async {
     _audioQueue = List.from(audioKeys);
@@ -23,16 +23,19 @@ class AudioPlaybackService {
 
   Future<void> _playNextAudio() async {
     if (_audioQueue.isEmpty || _shouldStop) {
+      currentPlayingAudioId = null; // Reset when queue is empty or stopped
       return;
     }
 
     String nextAudioKey = _audioQueue.removeAt(0);
+    currentPlayingAudioId = nextAudioKey; // Extract ID and assign
     await _playSingleAudio(nextAudioKey);
 
     if (_audioQueue.isNotEmpty && !_shouldStop) {
       for (int i = 0; i < 5; i++) {
         await Future.delayed(const Duration(milliseconds: 100));
         if (_shouldStop) {
+          currentPlayingAudioId = null; // Reset if stopped
           return;
         }
       }
@@ -54,6 +57,7 @@ class AudioPlaybackService {
 
       await _audioPlayer.onPlayerComplete.first;
       _isPlaying = false;
+      currentPlayingAudioId = null; // Clear after completion
     } else {
       throw Exception('Audio file not found in cache');
     }
@@ -63,16 +67,15 @@ class AudioPlaybackService {
     _shouldStop = true;
     _audioQueue.clear();
     await _audioPlayer.stop();
+    currentPlayingAudioId = null; // Clear when stopped
   }
 
   void dispose() {
     _audioPlayer.dispose();
   }
 
-  void listenForCompletion(VoidCallback onComplete) {
-    _audioPlayer.onPlayerComplete.listen((event) {
-      onComplete();
-    });
+  String? getCurrentPlayingAudioId() {
+    return currentPlayingAudioId;
   }
 
   bool isPlaying() {
